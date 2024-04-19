33dd280b4561cc795fcb950c702dc1826fe08213
Volkov Vyacheslav <akorp1@yandex.ru>
2024-04-19 17:09:57 +0300

    DZ2 End

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// пример: m = 2, n = 3 -> A(m,n) = 29
+
+// функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+
+int AckermannFunction(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (m == 0)
+    {
+        return AckermannFunction(n - 1, 1);
+    }
+    return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
+}
+
+Console.WriteLine("ВВЕДИТЕ НАТУРАЛЬНОЕ ЧИСЛО n ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ НАТУРАЛЬНОЕ ЧИСЛО m ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = AckermannFunction(num1, num2);
+Console.WriteLine(result);
